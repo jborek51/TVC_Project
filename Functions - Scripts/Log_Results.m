3f4621cda26a73c85553645a05ebdb04e8bf0e4c
John <jborek@uncc.edu>
2019-11-20 17:37:35 -0500

    Rebuilt plant model to incorporate coriolis terms and fixed angle of attack calculation.

--- Functions - Scripts/Log_Results.m
+++ Functions - Scripts/Log_Results.m
@@ -1,21 +1,21 @@
 %%  Log results
 Sim.time = VEH_out.time;
-Sim.Z_veh = VEH_out.signals.values(:,1);
-Sim.Z_dot_veh = VEH_out.signals.values(:,2);
-Sim.Z_ddot_veh = VEH_out.signals.values(:,3);
-Sim.Y_veh = VEH_out.signals.values(:,4);
-Sim.Y_dot_veh = VEH_out.signals.values(:,5);
-Sim.Y_ddot_veh = VEH_out.signals.values(:,6);
-Sim.theta_veh = VEH_out.signals.values(:,7)*180/pi;
-Sim.theta_dot_veh = VEH_out.signals.values(:,8)*180/pi;
+Sim.Z_veh = VEH_out.signals.values(:,6);
+Sim.Z_dot_veh = VEH_out.signals.values(:,8);
+% Sim.Z_ddot_veh = VEH_out.signals.values(:,3);
+Sim.Y_veh = VEH_out.signals.values(:,5);
+Sim.Y_dot_veh = VEH_out.signals.values(:,7);
+% Sim.Y_ddot_veh = VEH_out.signals.values(:,6);
+Sim.theta_veh = VEH_out.signals.values(:,11)*180/pi;
+Sim.theta_dot_veh = VEH_out.signals.values(:,10)*180/pi;
 Sim.theta_ddot_veh = VEH_out.signals.values(:,9)*180/pi;
 
-Sim.Z_b = VEH_out.signals.values(:,10);
-Sim.Z_dot_b = VEH_out.signals.values(:,11);
-Sim.Z_ddot_b = VEH_out.signals.values(:,12);
-Sim.Y_b = VEH_out.signals.values(:,13);
-Sim.Y_dot_b = VEH_out.signals.values(:,14);
-Sim.Y_ddot_b = VEH_out.signals.values(:,15);
+% Sim.Z_b = VEH_out.signals.values(:,10);
+Sim.Z_dot_b = VEH_out.signals.values(:,2);
+Sim.Z_ddot_b = VEH_out.signals.values(:,4);
+% Sim.Y_b = VEH_out.signals.values(:,13);
+Sim.Y_dot_b = VEH_out.signals.values(:,1);
+Sim.Y_ddot_b = VEH_out.signals.values(:,3);
 
 Sim.F_thrust = Dist_out.signals.values(:,1);
 Sim.Z_thrust = Dist_out.signals.values(:,2);
@@ -33,9 +33,10 @@ Sim.Norm = Dist_out.signals.values(:,12);
 Sim.delta = ACT_out.signals.values(:,1);
 Sim.Sat_diff = ACT_out.signals.values(:,2);
 Sim.V_app = V_app;
+Sim.V_a = V_a;
 Sim.V_appD = V_appD;
 Sim.V_appS = V_appS;
-Sim.theta_app = theta_app;
+Sim.alpha = alpha;
 Sim.SM = Stability;
 Sim.mass = VEH.m_lb;
 Sim.date = datestr(now, 'dd-mmmm_HH-MM');
--- Functions - Scripts/Plot_Results.m
+++ Functions - Scripts/Plot_Results.m
@@ -1,28 +1,34 @@
 %%
 % tFinal = Sim.time(end);
-tFinal = 4;
+tF = 4;
 V_W = round((ENV.v_w0+ENV.v_wf)/2/.447);
+idx = find(Sim.time == tF,1,'first');
 %%  Plot translational states
 figure()
 subplot(3,3,2)
 hold on;    grid on;
-plot(Sim.Y_veh,Sim.Z_veh,'k-')
-xlim([0 max(Sim.Y_veh)]);
-xlabel('Position [m]'); ylabel('Altitude [m]');
+% plot(Sim.Y_veh(1:idx),Sim.Z_veh(1:idx),'k-')
+plot(Sim.time,Sim.Z_veh,'c-')
+plot(Sim.time,Sim.Y_veh,'m-')
+% xlim([0 max(Sim.Y_veh)]);
+xlim([0 tF]);
+% xlabel('Position [m]'); ylabel('Altitude [m]');
+xlabel('Time [s]'); ylabel('Distance [m]');
 ax = gca;    ax.FontSize = 12;
 % text(20,10,'$v_w\longrightarrow$','FontSize',16)
 subplot(3,3,5)
 hold on;    grid on;
-plot(Sim.time,Sim.Z_dot_veh,'c-')
-plot(Sim.time,Sim.Y_dot_veh,'m-')
-xlim([0 tFinal]);
+plot(Sim.time,Sim.Z_dot_b,'c-')
+plot(Sim.time,Sim.Y_dot_b,'m-')
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('Velocity [m/s]');
+% legend('Z','Y');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,8)
 hold on;    grid on;
-plot(Sim.time,Sim.Z_ddot_veh,'c-')
-plot(Sim.time,Sim.Y_ddot_veh,'m-')
-xlim([0 tFinal]);
+plot(Sim.time,Sim.Z_ddot_b,'c-')
+plot(Sim.time,Sim.Y_ddot_b,'m-')
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('Acceleration [m/$\mathrm{s^2}$]');
 % L = legend('$Z$','$Y$');
 ax = gca;    ax.FontSize = 12;
@@ -32,18 +38,21 @@ ax = gca;    ax.FontSize = 12;
 subplot(3,3,3)
 hold on;    grid on;
 plot(Sim.time,Sim.theta_veh,'k-')
-xlim([0 tFinal]);
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('$\theta$ [$^o$]');
+ax = gca;    ax.FontSize = 12;
 subplot(3,3,6)
 hold on;    grid on;
 plot(Sim.time,Sim.theta_dot_veh,'k-')
-xlim([0 tFinal]);
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('$\omega$ [$^o/\mathrm{s}$]');
+ax = gca;    ax.FontSize = 12;
 subplot(3,3,9)
 hold on;    grid on;
 plot(Sim.time,Sim.theta_ddot_veh,'k-')
-xlim([0 tFinal]);
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('$\dot{\omega}$ [$^o/\mathrm{s}^2$]');
+ax = gca;    ax.FontSize = 12;
 %%  Plot Forces
 % figure()
 subplot(3,3,1)
@@ -51,23 +60,23 @@ hold on;    grid on;
 plot(Sim.time,Sim.F_thrust,'k-')
 plot(Sim.time,Sim.Z_thrust,'c--')
 plot(Sim.time,Sim.Y_thrust,'m--')
-xlim([0 tFinal]);
-ylabel('Thrust [N]');
+xlim([0 tF]);
+xlabel('Time [s]'); ylabel('Thrust [N]');
 legend('Nom','Z','Y');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,4)
 hold on;    grid on;
 plot(Sim.time,Sim.F_drag,'c-')
 plot(Sim.time,Sim.F_side,'m-')
-xlim([0 tFinal]);
-ylabel('Aero [N]');
+xlim([0 tF]);
+xlabel('Time [s]'); ylabel('Aero [N]');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,7)
 hold on;    grid on;
 plot(Sim.time,Sim.Z_thrust-Sim.F_drag-Sim.Z_grav+Sim.Norm,'c-')
 % plot(Sim.time,Sim.Z_thrust-Sim.F_drag-Sim.Z_grav,'c-')
 plot(Sim.time,Sim.Y_thrust+Sim.F_side-Sim.Y_grav,'m-')
-xlim([0 tFinal]);
+xlim([0 tF]);
 xlabel('Time [s]'); ylabel('$\sum F$ [N]');
 ax = gca;    ax.FontSize = 12;
 % T = suptitle(['TVC Simulation: $m$ = ',num2str(VEH.m_lb),'kg, $v_w$ = ',num2str(V_W),'mph, F15 Motor']);
