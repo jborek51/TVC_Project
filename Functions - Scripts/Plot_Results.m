efb9b9920f9d24208f139a280259d0ba28d39698
John <jborek@uncc.edu>
2019-11-25 15:26:27 -0500

    Fixed gravitational force vector

--- Functions - Scripts/Plot_Results.m
+++ Functions - Scripts/Plot_Results.m
@@ -1,35 +1,35 @@
 %%
 % tFinal = Sim.time(end);
-tF = 4;
+tF = 8;
 V_W = round((ENV.v_w0+ENV.v_wf)/2/.447);
 idx = find(Sim.time == tF,1,'first');
 %%  Plot translational states
 figure()
 subplot(3,3,2)
 hold on;    grid on;
-% plot(Sim.Y_veh(1:idx),Sim.Z_veh(1:idx),'k-')
-plot(Sim.time,Sim.Z_veh,'c-')
-plot(Sim.time,Sim.Y_veh,'m-')
-% xlim([0 max(Sim.Y_veh)]);
-xlim([0 tF]);
-% xlabel('Position [m]'); ylabel('Altitude [m]');
-xlabel('Time [s]'); ylabel('Distance [m]');
+% plot(Sim.time,Sim.Z_veh,'c-')
+% plot(Sim.time,Sim.Y_veh,'m-')
+% xlim([0 tF]);
+% xlabel('Time [s]'); ylabel('Distance [m]');
+plot(Sim.Y_veh(1:idx),Sim.Z_veh(1:idx),'k-')
+xlim([0 max(Sim.Y_veh)]);
+xlabel('Position [m]'); ylabel('Altitude [m]');
 ax = gca;    ax.FontSize = 12;
 % text(20,10,'$v_w\longrightarrow$','FontSize',16)
 subplot(3,3,5)
 hold on;    grid on;
-plot(Sim.time,Sim.Z_dot_b,'c-')
-plot(Sim.time,Sim.Y_dot_b,'m-')
+plot(Sim.time,Sim.Y_dot_b,'r-')
+plot(Sim.time,Sim.Z_dot_b,'b-')
 xlim([0 tF]);
 xlabel('Time [s]'); ylabel('Velocity [m/s]');
 % legend('Z','Y');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,8)
 hold on;    grid on;
-plot(Sim.time,Sim.Z_ddot_b,'c-')
-plot(Sim.time,Sim.Y_ddot_b,'m-')
+plot(Sim.time,Sim.Y_ddot_b,'r-')
+plot(Sim.time,Sim.Z_ddot_b,'b-')
 xlim([0 tF]);
-xlabel('Time [s]'); ylabel('Acceleration [m/$\mathrm{s^2}$]');
+xlabel('Time [s]'); ylabel('Body Accel [m/$\mathrm{s^2}$]');
 % L = legend('$Z$','$Y$');
 ax = gca;    ax.FontSize = 12;
 
@@ -57,25 +57,24 @@ ax = gca;    ax.FontSize = 12;
 % figure()
 subplot(3,3,1)
 hold on;    grid on;
-plot(Sim.time,Sim.F_thrust,'k-')
-plot(Sim.time,Sim.Z_thrust,'c--')
-plot(Sim.time,Sim.Y_thrust,'m--')
+plot(Sim.time,Sim.Y_thrust,'r-')
+plot(Sim.time,Sim.Z_thrust,'b-')
 xlim([0 tF]);
 xlabel('Time [s]'); ylabel('Thrust [N]');
-legend('Nom','Z','Y');
+legend('Y','Z');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,4)
 hold on;    grid on;
-plot(Sim.time,Sim.F_drag,'c-')
-plot(Sim.time,Sim.F_side,'m-')
+plot(Sim.time,Sim.F_side,'r-')
+plot(Sim.time,Sim.Y_grav,'b-')
 xlim([0 tF]);
-xlabel('Time [s]'); ylabel('Aero [N]');
+xlabel('Time [s]'); ylabel('Force [N]');
+legend('$F_\mathrm{side}$','$F_\mathrm{grav}^y$','location','northwest');
 ax = gca;    ax.FontSize = 12;
 subplot(3,3,7)
 hold on;    grid on;
-plot(Sim.time,Sim.Z_thrust-Sim.F_drag-Sim.Z_grav+Sim.Norm,'c-')
-% plot(Sim.time,Sim.Z_thrust-Sim.F_drag-Sim.Z_grav,'c-')
-plot(Sim.time,Sim.Y_thrust+Sim.F_side-Sim.Y_grav,'m-')
+plot(Sim.time,Sim.Y_thrust+Sim.F_side-Sim.Y_grav,'r-')
+plot(Sim.time,Sim.Z_thrust-Sim.F_drag-Sim.Z_grav+Sim.Norm,'b-')
 xlim([0 tF]);
 xlabel('Time [s]'); ylabel('$\sum F$ [N]');
 ax = gca;    ax.FontSize = 12;
